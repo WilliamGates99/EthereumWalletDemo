b8e7e6389c88f2c8f7321d119ec76a9c840a9b43
William Gates <william.gates.3299@gmail.com>
2023-08-18 21:27:19 +0330

    Implemented Room Database

--- app/src/main/java/com/xeniac/ethereumwalletdemo/feature_wallet/data/local/EthWalletDao.kt
+++ app/src/main/java/com/xeniac/ethereumwalletdemo/feature_wallet/data/local/EthWalletDao.kt
@@ -15,5 +15,5 @@ interface EthWalletDao {
     suspend fun deleteAllEthWallets()
 
     @Query("SELECT * FROM eth_wallets LIMIT 1")
-    suspend fun getFirstEthWalletInfo(): EthWalletInfoDto
+    suspend fun getFirstEthWalletInfo(): EthWalletInfoDto?
 }
